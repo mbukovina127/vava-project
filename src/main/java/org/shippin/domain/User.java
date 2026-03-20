101c8d0f72ab186ca0da022e2c51eecf3d73a88b
ZEPHY <xbukovinam@stuba.sk>
2026-03-21 00:28:17 +0100

    Fix: fixed user Dao test

--- src/main/java/org/shippin/domain/User.java
+++ src/main/java/org/shippin/domain/User.java
@@ -14,5 +14,11 @@ public class User {
     private String email;
     private Role role;
 
+    public User(String name, String email, Role role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
 }
 
