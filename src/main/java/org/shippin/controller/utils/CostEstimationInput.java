850413bcb9c0036ef4bdc7b345aeb385f230d6f8
Marcel Novomesky <marnov042@gmail.com>
2026-05-01 19:42:41 +0200

    added warehouseId and remvoed shipment_opt from CostEstimationInput

--- src/main/java/org/shippin/controller/utils/CostEstimationInput.java
+++ src/main/java/org/shippin/controller/utils/CostEstimationInput.java
@@ -5,12 +5,12 @@ import java.util.List;
 public record CostEstimationInput(
         String date,
         String from,
+        int warehouseId,
         String destination,
         double weight,
         double volume,
         double fuelSurcharge,
         double toll,
-        String shipment_opt,
         String delivery_opt,
         List<ExtraOption> options
 ) {}
