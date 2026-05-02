2ffc9b38fcdf88f55b9adf532b23217c71aa7808
Martin Kvietok <105906086+0MarrkvI0@users.noreply.github.com>
2026-05-02 23:28:31 +0200

    Vava-56-frontend+CostBreakdown (#72)
    * Shipment processing implementation (#60)
    
    * added small price list path
    
    * changed test for postal_codes, from "Postal codes:" to "Locations:"
    
    * added warehouseId and remvoed shipment_opt from CostEstimationInput
    
    * removed shipment Type selection
    
    * removed SMALL_PACKAGE and SHIPMENT
    added serviceId mapping
    
    * removed shipment type toggle, date (will be set to current timestamp)
    added warehouse list inicialization + serviceId init
    
    * added shipment creation, and saving to database
    removed SMALL_PACKAGE options
    
    * adapted new dao refarcor
    moved shipment calculation from CostBreakdownController to CostEstimationController
    
    * refactored estimateCost() from Shipment to ShipmentService
    
    * VAVA-81 WIP shipment details (#62)
    
    * Lot of changes:
    
    ShipmentDetails now works with Shipment but didn't test it yet we also have a problem with missing testing data in database
    
    Refractored Maps into a seperate service
    
    Shipment has new weight and volume attributes and updated the dao and a script with the changes
    
    also changed Cost estimation controller and cost breakdown to accept the Shipment object
    
    * Removed old depricated Session service
    
    ---------
    
    Co-authored-by: Marcel Novomesky <marnov042@gmail.com>
    Co-authored-by: mbukovina127 <145677853+mbukovina127@users.noreply.github.com>
    
    * Warehouse implementation wip (#64)
    
    * Half done edit warehouse screen, warehouse management screen, and edit
    small package list screen.
    
    * Merge branch 'VAVA-Parser-Marcel' of
    https://github.com/mbukovina127/vava-project.git into VAVA-Parser-Marcel
    
    * FIX: db creation (#66)
    
    FEAT: inserts added
    
    * VAVA-81 Shipment detail mostly implemented (#67)
    
    * lots of changes
    
     and refractors of DAOs
    
    and logging
    
    * fix/ float casting
    
    * Refactor: cleaning css,buttons,logo
    
    * Refactor: CostBreakdown partial sums in rows (including Servicies)
    
    ---------
    
    Co-authored-by: xnovomesky <xnovomesky@stuba.sk>
    Co-authored-by: Marcel Novomesky <marnov042@gmail.com>
    Co-authored-by: mbukovina127 <145677853+mbukovina127@users.noreply.github.com>
    Co-authored-by: Juraj Černička <145677632+xcernickaj@users.noreply.github.com>
    Co-authored-by: Filip Brutovský <128379193+AweFilko@users.noreply.github.com>

--- src/main/java/org/shippin/controller/MenuController.java
+++ src/main/java/org/shippin/controller/MenuController.java
@@ -67,6 +67,7 @@ public class MenuController implements Initializable {
     // CONTENT
     @FXML private StackPane contentArea;
     private Screens currentScreen;
+    private Object  currentData;
 
     public void showOverlay(javafx.scene.Node content) {
     	if (content instanceof Region region) {
@@ -92,6 +93,7 @@ public class MenuController implements Initializable {
             return;
         }
         currentScreen = screen;
+        currentData   = data;
         try {
             URL fxmlUrl = getClass().getResource(Screens.resolveScreen(screen));
             if (fxmlUrl == null) {
@@ -124,9 +126,7 @@ public class MenuController implements Initializable {
     public void initialize(URL location, ResourceBundle resources)
     {
         langButton.setText(NavigationService.getBundle().getLocale().getLanguage().equals("sk") ? "EN" : "SK");
-
-        //TODO dat meno usera zo session (get string)
-        //UserNameLabel.setText(username);
+        UserNameLabel.setText(UserService.getUser().getFullUserName());
 
         List<Button> buttons = new ArrayList<>();
 
@@ -185,62 +185,6 @@ public class MenuController implements Initializable {
 
             leftSidebar.getChildren().add(btn);
         }
-
-
-//        //..TOTO ZMAZAT OD TADE
-//        Button testBtn = new Button();
-//        testBtn.getStyleClass().add("sidebar-btn");
-//
-//        var testStream = getClass().getResourceAsStream("/icons/png-dark/plus_black.png");
-//        if (testStream != null) {
-//            ImageView testIcon = new ImageView(new Image(testStream));
-//            testIcon.setFitWidth(40);
-//            testIcon.setFitHeight(40);
-//            testIcon.setPreserveRatio(true);
-//            testBtn.setGraphic(testIcon);
-//        }
-//
-//        VBox.setMargin(testBtn, new Insets(10, 5, 0, 5));
-//
-//        testBtn.setOnAction(e -> {
-//            System.out.println("TEST BUTTON CLICKED!");
-//            try {
-//                URL fxmlUrl = getClass().getResource("/views/ShipmentDetail.fxml");
-//                FXMLLoader loader = new FXMLLoader(fxmlUrl);
-//                loader.setResources(NavigationService.getBundle());
-//                Node node = loader.load();
-//
-//                Object ctrl = loader.getController();
-//                if (ctrl instanceof BaseController<?> bc) {
-//                    bc.setMenuController(this);
-//                }
-//
-//                if (ctrl instanceof ShipmentDetailController sdc) {
-//                    CostEstimationInput est = new CostEstimationInput(
-//                            "2026-04-30",           // date
-//                            "Bratislava",           // from
-//                            "09301",                // destination ← PSČ KOŠICE!
-//                            54.46,                  // weight
-//                            210,                    // volume
-//                            5.0,                    // fuelSurcharge
-//                            2.0,                    // toll
-//                            "TEST",                 // shipment_opt
-//                            "TEST",                 // delivery_opt
-//                            List.of()               // options
-//                    );
-//                    ShipmentData dummyData = new ShipmentData(est, "Test", 321);
-//                    sdc.onData(dummyData);
-//                }
-//
-//                contentArea.getChildren().setAll(node);
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//        });
-//
-//        leftSidebar.getChildren().add(testBtn);
-//        //PO TADE
-
         loadScreen(NAV_ITEMS.getFirst().screen(),null);
     }
 
@@ -254,27 +198,12 @@ public class MenuController implements Initializable {
         NavigationService.setLocale(next);
         langButton.setText(next.getLanguage().equals("sk") ? "EN" : "SK");
         if (currentScreen != null) {
-            loadScreen(currentScreen, null);
+            loadScreen(currentScreen, currentData);
         }
     }
 
-    private void setActive(Button activeBtn) {
-        // remove active from all
-//        sidebarBtn1.getStyleClass().remove("sidebar-btn-active");
-//        sidebarBtn2.getStyleClass().remove("sidebar-btn-active");
-//        sidebarBtn3.getStyleClass().remove("sidebar-btn-active");
-//        sidebarBtn4.getStyleClass().remove("sidebar-btn-active");
-//        sidebarBtn5.getStyleClass().remove("sidebar-btn-active");
-//        sidebarBtn6.getStyleClass().remove("sidebar-btn-active");
-//
-//        // add normal class back (optional safety)
-//        sidebarBtn1.getStyleClass().add("sidebar-btn");
-//        sidebarBtn2.getStyleClass().add("sidebar-btn");
-//        sidebarBtn3.getStyleClass().add("sidebar-btn");
-//        sidebarBtn4.getStyleClass().add("sidebar-btn");
-//        sidebarBtn5.getStyleClass().add("sidebar-btn");
-//        sidebarBtn6.getStyleClass().add("sidebar-btn");
-
+    private void setActive(Button activeBtn)
+    {
         // remove normal from clicked
         activeBtn.getStyleClass().remove("sidebar-btn");
 
--- src/main/java/org/shippin/database/dao/BaseDAO.java
+++ src/main/java/org/shippin/database/dao/BaseDAO.java
@@ -2,6 +2,7 @@ package org.shippin.database.dao;
 
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import org.shippin.database.DBConnector;
 
 public abstract class BaseDAO {
@@ -12,4 +13,15 @@ public abstract class BaseDAO {
         this.connection = DBConnector.getInstance().getConnection();
     }
 
+    public void setAutoCommit(boolean autoCommit) throws SQLException {
+        connection.setAutoCommit(autoCommit);
+    }
+
+    public void commit() throws SQLException {
+        connection.commit();
+    }
+
+    public void rollback() throws SQLException {
+        connection.rollback();
+    }
 }
